da85d866f685ea74f49c253057eaf65edbfb5e50
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-02-04 16:14:25 +0000

    .

--- debugging.sh
+++ debugging.sh
@@ -1,1 +1,1 @@
-make clean && make && g++ -std=c++11 -I/home/daniel-robson/Projects/intlen/inc -Iinc -L/home/daniel-robson/Projects/intlen/lib -Llib -Wall -DARC64 -o bin/debugging.exec debugging.cpp -ltagged_memory -lintlen && clear && ./bin/debugging.exec
+make clean && make && g++ -std=c++11 -Iinc -Llib -Wall -DARC64 -o bin/debugging.exec debugging.cpp -ltagged_memory -lintlen && clear && ./bin/debugging.exec
